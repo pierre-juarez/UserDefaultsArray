8ac32f0336518307c21a9333aac2661751275759
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-05-29 09:57:38 -0500

    [add] - Save array in UserDefaults 🤓

--- UserDefaultsArray/ViewController.swift
+++ UserDefaultsArray/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txt1: UITextField!
+    @IBOutlet weak var txt2: UITextField!
+    @IBOutlet weak var txt3: UITextField!
+    
+    var array = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let myArray = UserDefaults.standard.stringArray(forKey: "arrayUD"){
+            txt1.text = myArray[0]
+            txt2.text = myArray[1]
+            txt3.text = myArray[2]
+        }
     }
 
-
+    
+    @IBAction func btnSave(_ sender: Any) {
+        if txt1.text == ""{
+            alertCustom(title: "Error", message: "Llena el primer campo")
+            return
+        }
+        if txt2.text == ""{
+            alertCustom(title: "Error", message: "Llena el segundo campo")
+            return
+        }
+        if txt3.text == ""{
+            alertCustom(title: "Error", message: "Llena el tercer campo")
+            return
+        }
+        
+        array.append(txt1.text ?? "")
+        array.append(txt2.text ?? "")
+        array.append(txt3.text ?? "")
+        
+        UserDefaults.standard.set(array, forKey: "arrayUD")
+        
+        alertCustom(title: "Completado", message: "Se guardaron correctamente los valores")
+    }
+    
+    func alertCustom(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let btn = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(btn)
+        present(alert, animated: true)
+    }
 }
 
